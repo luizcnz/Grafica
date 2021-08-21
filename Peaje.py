508b5b9b0d893d45b1822f628578a5d5a7d251fc
Sumomo Aigasaki <sumomoaigasaki@outlook.com>
2021-08-20 21:13:47 -0600

    arregle disenio y agrege mas botonoes a peaje

--- Peaje.py
+++ Peaje.py
@@ -4,7 +4,7 @@ import tkinter as tk
 import os
 from tkinter.constants import RIGHT, TOP, X
 import tkinter.font as TkFont
-from tkinter import ttk
+from tkinter import Frame, Label, ttk
 import time
 import tkinter.messagebox as MessageBox
 import tkinter.scrolledtext as ScrolledText
@@ -15,12 +15,14 @@ import time
 ventana = tk.Tk()
 ventana.geometry("1250x750")
 ventana.title("Peajes")
+ventana.resizable(0,0)
 
 # Configuracion de texto
 texto=TkFont.Font(family="Calibri bold italic",size=14)
 textoEtiqueta= TkFont.Font(family="Calibri italic",size=14)
 textobotones=TkFont.Font(family="Calibri italic",size=12)
-
+textoEnunciado=TkFont.Font(family="Calibri bold italic",size=12)
+textoResultado=TkFont.Font(family="Calibri italic",size=12)
 
 Label_Titulo=tk.Label(ventana,text="Cobro de Peaje",font=texto).place(relx=0.01, rely=0.01, relheight=0.05, relwidth=1)
 
@@ -49,15 +51,33 @@ imgSearch=tk.PhotoImage(file="iconos/buscar32.png")
 btnSearch=tk.Button(marcoCentral,image=imgSearch,text=" Buscar ", font=textobotones,compound="left").place(relx=0.38, rely=0.88)
 
 #------------------------------------REPORTE -------------------------------------------------------
-panelReporte = tk.Label(marcoCentral,bg="#CFCFCF").place(relx=0.51, rely=0.18,relheight=0.30, relwidth=0.47)
+frameReport = Frame()
+frameReport.place(relx=0.51, rely=0.18,relheight=0.30, relwidth=0.47)
+frameReport.config(bg="#CFCFCF")
+lblOwner= Label(frameReport,text="Titular del Vehiculo: ",font=textoEnunciado, bg="#CFCFCF").place(relx=0.01, rely=0.03)
+lblOwnerResult= Label(frameReport,text="Prueba ",font=textoResultado, bg="#CFCFCF").place(relx=0.30, rely=0.03)
+
+lblUbication= Label(frameReport,text="Ubicacion: ",font=textoEnunciado, bg="#CFCFCF").place(relx=0.70, rely=0.03)
+lblUbicationResult= Label(frameReport,text="SPS ",font=textoResultado, bg="#CFCFCF").place(relx=0.85, rely=0.03)
+
+lblReport= Label(frameReport,text="Reporte: ",font=textoEnunciado, bg="#CFCFCF").place(relx=0.01, rely=0.17)
+lblReportResult= Label(frameReport,text="Sin Reporte ",font=textoResultado, bg="#CFCFCF").place(relx=0.30, rely=0.17)
+
+
+#panelReporte = tk.Label(marcoCentral,bg="#CFCFCF").place(relx=0.51, rely=0.18,relheight=0.30, relwidth=0.47)
 #EL place la X se mueve para de izquierda a derecha
 imgTicket=tk.PhotoImage(file="iconos/boleto32.png")
-btnTicket=tk.Button(marcoCentral,image=imgTicket,text=" Ticket ", font=textobotones,compound="left").place(relx=0.91, rely=0.49)
+btnTicket=tk.Button(marcoCentral,image=imgTicket,text=" Ver Tickets ", font=textobotones,compound="left").place(relx=0.51, rely=0.49)
+
+imgNew=tk.PhotoImage(file="iconos/check.png")
+btnTicket=tk.Button(marcoCentral,image=imgNew,text=" Nuevo Ticket ", font=textobotones,compound="left").place(relx=0.70, rely=0.49)
+
+imgFactura=tk.PhotoImage(file="iconos/cuenta32.png")
+btnFactura=tk.Button(marcoCentral,image=imgFactura,text=" Factura ", font=textobotones,compound="left").place(relx=0.89, rely=0.49)
+
 #-------------------------------------------------Imprimir------------------------------------------------
 
 panelPrint = tk.Label(marcoCentral,bg="#CFCFCF").place(relx=0.51, rely=0.55,relheight=0.30, relwidth=0.47)
-imgFactura=tk.PhotoImage(file="iconos/cuenta32.png")
-btnFactura=tk.Button(marcoCentral,image=imgFactura,text=" Factura ", font=textobotones,compound="left").place(relx=0.51, rely=0.88)
 
 imgRefresh=tk.PhotoImage(file="iconos/recargar.png")
 btnRefresh=tk.Button(marcoCentral,image=imgRefresh,text=" Nuevo ", font=textobotones,compound="left").place(relx=0.90, rely=0.88)
