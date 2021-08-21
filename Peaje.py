df4fb9ee2e547c3be68c5540677a130d58f2c782
Luiz <cluisromero@hotmail.com>
2021-08-21 14:32:06 -0600

    programa casi completo falta  corregir factu

--- Peaje.py
+++ Peaje.py
@@ -38,7 +38,7 @@ textoplaca = 'HBE 9052'
 # Area de funciones
 
 def buscar():
-
+    global textoplaca
     if cmbUbication.get()=="Marcovia":
         ubicacionPeaje=1
     if cmbUbication.get()=="Choluteca":
@@ -150,8 +150,132 @@ def buscar():
     database.seleccionar()
 
 
+def vertickets():
+    global textoplaca
+    if cmbUbication.get()=="Marcovia":
+        ubicacionPeaje=1
+    if cmbUbication.get()=="Choluteca":
+        ubicacionPeaje=2
+    if cmbUbication.get()=="San Lorenzo":
+        ubicacionPeaje=3
+
+    print("Lugar de peaje: ",cmbUbication.get())
+    print("ID del Lugar de peaje: ",ubicacionPeaje)
+    lblUbicationResult.configure(text=cmbUbication.get())
+
+    #deteccion de la placa del auto cuando se rotan o buscan----------------------------
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.blur(gray,(3,3))
+    canny = cv2.Canny(gray,150,200)
+    cnts,_ = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+    vuelta = 0
+    for c in cnts:
+        #contar=contar+1
+        area = cv2.contourArea(c)
+
+        x,y,w,h = cv2.boundingRect(c)
+        epsilon = 0.05*cv2.arcLength(c,True)
+        approx = cv2.approxPolyDP(c,epsilon,True)
+        
+        if len(approx)==4 and area>9000:
+            print('area=',area)
+            vuelta=vuelta+1
+            
+            aspect_ratio = float(w)/h
+            
+            if aspect_ratio>2:
+                placa = gray[y:y+h,x:x+w]
+                txt = pytesseract.image_to_string(placa,config='--psm 10')
+                split_string = txt.split()
+                textoplaca = split_string[0]+" "+split_string[1]
+    
+                lblPlacaText.configure(text=textoplaca)
+                cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
+                cv2.putText(image,textoplaca,(x-20,y-10),1,2.2,(0,255,0),3)
+                # panel.configure(image=image)
+                # panel.image = image
+                #imshow("imagen",image)
+
+    #fin de la deteccion de la placa del auto ------------------------
+
+    class DataBase:
+        def __init__(self):#coneccion con la base de datos
+            self.connection=pymysql.connect(host='173.249.21.6', user='movil2',password='carwash2021',db='ProyectoFinal_Python')
+
+            self.cursor = self.connection.cursor()
+            print("Conexion Exitosa!!!")
+
+        def seleccionar(self):#inseercion de datos
+            div = textoplaca.split()
+            dbplaca = div[0]+" "+div[1]
+            sql='SELECT Entidad.Nombre, Entidad.Ubicacion, Vehiculos.IDVehiculos FROM Entidad INNER JOIN Vehiculos ON Vehiculos.IdEntidad = Entidad.IdEntidad  where Placa = "'+str(dbplaca)+'"'
+
+            print("query: ",sql)
+            try:
+                self.cursor.execute(sql)
+                self.connection.commit()
+                rows = self.cursor.fetchall()
+                for row in rows:
+                    print(row)
+                
+                entidadnombre = row[0]
+                entidadlugar = row[1]
+                vehiculoid=row[2]
+                print("Datos obtenidos, Nombre: ",entidadnombre, "Lugar: ", entidadlugar)
+
+                lblOwnerResult.configure(text=entidadnombre)
+                lblUbicationResult.configure(text=entidadlugar)
+            
+            except Exception as e:
+                raise
+
+            sql2='SELECT Fecha, Subtotal, Estado_Pago from Ticket  WHERE idVehiculos =  "'+str(vehiculoid)+'" and Estado_Pago=0'
+            print("query2: ",sql2)
+            
+            ticketfecha=[]
+            ticketsub=[]
+            totalticket=0
+
+            try:
+                self.cursor.execute(sql2)
+                self.connection.commit()
+                rows = self.cursor.fetchall()
+                indexlist=1
+                for row in rows:
+                    print(row)
+                    totalticket=totalticket+row[1]
+                    ticketfecha.append( row[0])
+                    ticketsub.append( row[1])
+                    listdata = "Fecha: "+str(row[0])+" - Monto: "+str(row[1])+" - Placa: "+ str(dbplaca)
+                    lstTicketResult.insert(indexlist,listdata)
+                
+                lblTicketResult.config(text=str(totalticket)+"Lps.")
+                print("Fechas de los tickets:",ticketfecha)
+                print("Subtotal de los tickets: ",ticketsub)
+
+                
+                indexlist=+1
+            except Exception as e:
+                raise
+
+    print('PLACA: ',textoplaca)
+            
+    database = DataBase()
+    database.seleccionar()
+
+
 #Funcion para cambiar la imagen del vehiculo con una nueva
 def rotar():
+
+    lblTicketResult.configure(text="")
+    lblOwnerResult.configure(text="")
+    lblUbicationResult.configure(text="")
+
+    lblReportResult.delete(0,END)
+    lstTicketResult.delete(0,END)
+
     global ubicacionPeaje
     global textoplaca
     global num
@@ -185,6 +309,7 @@ def rotar():
 
 #Funcion para generar un ticket que se guardarae en la base de datos
 def ticket():
+    global textoplaca
     graytemplate = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.blur(gray,(3,3))
@@ -577,7 +702,7 @@ lblReportResult.place(relx=0.15, rely=0.17)
 #panelReporte = tk.Label(marcoCentral,bg="#CFCFCF").place(relx=0.51, rely=0.18,relheight=0.30, relwidth=0.47)
 #EL place la X se mueve para de izquierda a derecha
 imgTicket=tk.PhotoImage(file="iconos/boleto32.png")
-btnTicket=tk.Button(marcoCentral,image=imgTicket,text=" Ver Tickets ", font=textobotones,compound="left").place(relx=0.51, rely=0.49)
+btnTicket=tk.Button(marcoCentral,image=imgTicket,text=" Ver Tickets ", command=vertickets, font=textobotones,compound="left").place(relx=0.51, rely=0.49)
 
 imgNew=tk.PhotoImage(file="iconos/check.png")
 btnTicket=tk.Button(marcoCentral,image=imgNew,text=" Nuevo Ticket ", command=ticket,font=textobotones,compound="left").place(relx=0.70, rely=0.49)
@@ -587,7 +712,18 @@ btnFactura=tk.Button(marcoCentral,image=imgFactura,text=" Factura ", command=fac
 
 #-------------------------------------------------Imprimir------------------------------------------------
 
-panelPrint = tk.Label(marcoCentral,bg="#CFCFCF").place(relx=0.51, rely=0.55,relheight=0.30, relwidth=0.47)
+#panelPrint = tk.Label(marcoCentral,bg="#CFCFCF").place(relx=0.51, rely=0.55,relheight=0.30, relwidth=0.47)
+frameTicket = Frame()
+frameTicket.place(relx=0.51, rely=0.55,relheight=0.30, relwidth=0.47)
+frameTicket.config(bg="#CFCFCF")
+
+lblTicket= Label(frameTicket,text="Total a Pagar por tickets pendientes de pago: ",font=textoEnunciado, bg="#CFCFCF").place(relx=0.01, rely=0.03)
+lblTicketResult= Label(frameTicket,text="",font=textoResultado, bg="#CFCFCF")
+lblTicketResult.place(relx=0.55, rely=0.03)
+
+lstTicketResult= Listbox(frameTicket,font=textoResultado, bg="#CFCFCF", width=70, height=8)
+lstTicketResult.place(relx=0.01, rely=0.17)
+
 
 imgRefresh=tk.PhotoImage(file="iconos/recargar.png")
 btnRefresh=tk.Button(marcoCentral,image=imgRefresh,text=" Nuevo ", command=rotar, font=textobotones,compound="left").place(relx=0.90, rely=0.88)
